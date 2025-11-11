ae40a090c5be6989830f37b2bc4c729e0ba80ac8
ARUN-KUMAR-YARLAGADDA <arunkumar_yarlagadda@srmap.edu.in>
2025-11-11 12:43:02 +0530

    Updated user routes in backend

---
+++ backend/controllers/userController.js
@@ -0,0 +1,39 @@
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password)
+        return res.status(400).json({ message: "All fields are required" });
+
+    const exists = await User.findOne({ email });
+    if (exists) return res.status(400).json({ message: "User already exist" });
+
+    const user = await User.create({ name, email, password });
+    res.status(201).json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user.id),
+    });
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user.id),
+        });
+    } else {
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
+});
+
+export { registerUser, loginUser };
