2c0d1425a212bc5b7bae4d582a0736d01f7018de
ARUN-KUMAR-YARLAGADDA <arunkumar_yarlagadda@srmap.edu.in>
2025-11-14 18:24:47 +0530

    removed strictmode and .env to gitignore

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
 import { BrowserRouter } from 'react-router-dom';
@@ -7,21 +6,19 @@ import { AuthProvider } from './contexts/AuthContext';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <BrowserRouter>
-            <AuthProvider>
-                <App />
-                <Toaster
-                    position="bottom-right"
-                    toastOptions={{
-                        style: {
-                            background: '#1a1f3a',
-                            color: '#fff',
-                            border: '1px solid #334155',
-                        },
-                    }}
-                />
-            </AuthProvider>
-        </BrowserRouter>
-    </StrictMode>,
+    <BrowserRouter>
+        <AuthProvider>
+            <App />
+            <Toaster
+                position="bottom-right"
+                toastOptions={{
+                    style: {
+                        background: '#1a1f3a',
+                        color: '#fff',
+                        border: '1px solid #334155',
+                    },
+                }}
+            />
+        </AuthProvider>
+    </BrowserRouter>,
 );
